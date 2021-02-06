8f9159a2528e9a2f18a49351bf3b7fefcc8b8213
Miguel Vilhena <mvmories@gmail.com>
2021-02-06 23:36:22 +0000

    broken our homepage into smaller components and fininshed styling our menu components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import Homepage from './Homepage.component';
+import Homepage from './pages/homepage/homepage.component';
 
 function App() {
   return (
