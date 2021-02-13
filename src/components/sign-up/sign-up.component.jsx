817f3a0a63e58cb952ae9794448740653e36bf68
Miguel Vilhena <mvmories@gmail.com>
2021-02-13 18:53:43 +0000

    finished sign-in handleSubmit method

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -58,7 +58,7 @@ class SignUp extends React.Component {
 
     return(
       <div className='sign-up'>
-      <h2 className="title">I do not have a account</h2>
+      <h2 className="title">I do not have an Account</h2>
       <span>Sign up with your email and password</span>
       <form className='sign-up-form' onSubmit={this.handleSubmit}>
         <FormInput
